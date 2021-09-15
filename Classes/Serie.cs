89e4af9225a16aa18e4ee0e817511b4a2d2bf5dd
raulalvesre <raul.alves.re@gmail.com>
2021-09-14 23:20:24 -0300

    fix: Correct indentation and formatting

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -4,28 +4,29 @@ namespace Raulflix.Classes
 {
     public class Serie : EntidadeBase
     {
-		public Genero Genero { get; private set; }
-		public string Titulo { get; private set; }
-		public string Descricao { get; private set; }
-		public int Ano { get; private set; }
-        public bool Excluido {get; private set;}
+        public Genero Genero { get; private set; }
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public int Ano { get; private set; }
+        public bool Excluido { get; private set; }
 
-		public Serie(int id, Genero genero, string titulo, string descricao, int ano)
-		{
-			Id = id;
-			Genero = genero;
-			Titulo = titulo;
-			Descricao = descricao;
-			Ano = ano;
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
             Excluido = false;
-		}
+        }
 
-        public void Excluir() {
+        public void Excluir()
+        {
             this.Excluido = true;
         }
 
         public override string ToString()
-		{
+        {
             return $@"
             Gênero: {Genero}
             Titulo: {Titulo}
--- Interfaces/IRepositorio.cs
+++ Interfaces/IRepositorio.cs
@@ -4,11 +4,11 @@ namespace Raulflix.Interfaces
 {
     public interface IRepositorio<T>
     {
-         List<T> Lista();
-         T RetornaPorId(int id);
-         void Insere(T entidade);
-         void Exclui(int id);
-         void Atualizar(int id, T entidade);
-         int ProximoId();
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualizar(int id, T entidade);
+        int ProximoId();
     }
 }
